e59a25fd7a01e1e59471e3c865896d77b1356478
DmitryK96 <sieger1012@gmail.com>
2026-05-12 14:46:31 +0700

    Начало реализации методов конвертации и настройки конвертера

---
+++ src/Image/TextGraphicsConverterImpl.java
@@ -0,0 +1,10 @@
+package Image;
+
+public class TextGraphicsConverterImpl {
+
+    // Реализация методов конвертации и настройки конвертера
+    private int maxWidth = 600;
+    private int maxHeight = 600;
+    private double maxRatio = 2.0;
+    private TextColorSchema schema = new SimpleTextColorSchema();
+}
