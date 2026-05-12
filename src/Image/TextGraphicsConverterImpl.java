8234160dc1ed4f40c5bebacd0bb4490abd9c2aeb
DmitryK96 <sieger1012@gmail.com>
2026-05-12 15:59:13 +0700

    Проверка соотношения сторон, Вычисление масштаба и Формирование ASCII-графики Реализация полного запуска сервера и ссылки на порт 8888 с конвертацией изображения по URL ссылке

--- src/Image/TextGraphicsConverterImpl.java
+++ src/Image/TextGraphicsConverterImpl.java
@@ -1,10 +1,74 @@
 package Image;
 
-public class TextGraphicsConverterImpl {
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+public class TextGraphicsConverterImpl implements TextGraphicsConverter {
 
     // Реализация методов конвертации и настройки конвертера
-    private int maxWidth = 600;
-    private int maxHeight = 600;
+    private int maxWidth = 736;
+    private int maxHeight = 736;
     private double maxRatio = 2.0;
     private TextColorSchema schema = new SimpleTextColorSchema();
+
+    @Override
+    public String convert(String url) throws IOException, BadImageSizeException{
+        BufferedImage img = ImageIO.read(new URL(url)); // Загрузка изображения по URL
+
+        // Получаем новые размеры
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        // Проверка соотношения сторон
+        double ratio = (double) width / height;
+        if (ratio > maxRatio) {
+            throw new BadImageSizeException(ratio, maxRatio);
+        }
+
+        // Вычисление масштаба
+        double scale = Math.min((double) maxWidth / width, (double) maxHeight / height);
+        int newWidth = (int) (width * scale);
+        int newHeight = (int) (height * scale);
+
+        Image scaledImage = img.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH); // Уменьшение картинки
+        BufferedImage bwImg = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_BYTE_GRAY); // Создание черно-белого изображения
+        Graphics2D g2d = bwImg.createGraphics(); // Рисование уменьшенного изображения
+        g2d.drawImage(scaledImage, 0, 0, null);
+        g2d.dispose();
+
+        // Формирование ASCII-графики (проход по пикселям циклом)
+        StringBuilder result = new StringBuilder();
+        for (int y = 0; y < newHeight; y++) {
+            for (int x = 0; x < newWidth; x++) {
+                int color = new Color(bwImg.getRGB(x, y)).getRed(); // Получение яркости пикселя
+                char c = schema.convert(color); // Преобразование яркости в символ
+                result.append(c).append(c); // Добавление символа
+            }
+            result.append('\n');
+        }
+        return result.toString();
+    }
+
+    @Override
+    public void setMaxWidth(int width) {
+        this.maxWidth = width;
+    }
+
+    @Override
+    public void setMaxHeight(int height) {
+        this.maxHeight = height;
+    }
+
+    @Override
+    public void setMaxRatio(double maxRatio) {
+        this.maxRatio = maxRatio;
+    }
+
+    @Override
+    public void setTextColorSchema(TextColorSchema schema) {
+        this.schema = schema;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 import Image.TextGraphicsConverter;
+import Image.TextGraphicsConverterImpl;
 import Server.GServer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TextGraphicsConverter converter = null;
+        TextGraphicsConverter converter = new TextGraphicsConverterImpl();
 
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем сервер
